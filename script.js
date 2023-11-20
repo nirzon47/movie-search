fb50fd75c4141c17cd315c71ec6b298b6f1df003
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-20 20:36:51 +0530

    UI optimizations and added fetch for the movie search

---
+++ script.js
@@ -0,0 +1,25 @@
+// DOM elements
+const movieSearchInput = document.getElementById('movie-search')
+
+// Variables
+let page = 1
+
+// Event listeners
+let timeout
+movieSearchInput.addEventListener('input', () => {
+	clearTimeout(timeout)
+
+	timeout = setTimeout(() => {
+		fetchMovies()
+	}, 1000)
+})
+
+// Functions
+const fetchMovies = async (page) => {
+	const response = await fetch(
+		`https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieSearchInput.value}&type=movie&page=${page}`
+	)
+
+	const data = await response.json()
+	console.log(data)
+}
