a1594c0d424c9cc1ccf8366dafce216c5cf80f35
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-20 22:41:09 +0530

    added movie rendering

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // DOM elements
 const movieSearchInput = document.getElementById('movie-search')
+const movieListSection = document.getElementById('movie-list')
 
 // Variables
 let page = 1
@@ -20,10 +21,53 @@ const fetchMovies = async (page) => {
 		const response = await fetch(
 			`https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieSearchInput.value}&type=movie&page=${page}`
 		)
-
 		const data = await response.json()
+
 		renderMovies(data)
 	} catch (error) {
 		console.log(error)
 	}
+}
+
+const renderMovies = (data, page = 1) => {
+	movieListSection.style.opacity = 0
+	const fragment = document.createDocumentFragment()
+
+	data.Search.forEach((movie) => {
+		const movieContainerDiv = document.createElement('div')
+		movieContainerDiv.classList.add('movie-container')
+
+		const imageContainerDiv = document.createElement('div')
+		imageContainerDiv.classList.add('image-container')
+
+		if (movie.Poster === 'N/A') {
+			imageContainerDiv.innerHTML = `<img
+                class="poster"
+                src="https://via.placeholder.com/300x450"
+                alt="${movie.Title}"
+            />`
+		} else {
+			imageContainerDiv.innerHTML = `<img
+                class="poster"
+                src="${movie.Poster}"
+                alt="${movie.Title}"
+            />`
+		}
+
+		const subtitleDiv = document.createElement('div')
+		subtitleDiv.classList.add('text-center')
+		subtitleDiv.innerHTML = `<h3 class="movie-heading">${movie.Title}</h3>
+                                    <p class="text-sm">
+                                    Released <span class="font-semibold text-accent">${movie.Year}</span>
+                                    </p>`
+
+		movieContainerDiv.appendChild(imageContainerDiv)
+		movieContainerDiv.appendChild(subtitleDiv)
+
+		fragment.appendChild(movieContainerDiv)
+	})
+
+	movieListSection.innerHTML = ''
+	movieListSection.appendChild(fragment)
+	movieListSection.style.opacity = 1
 }
